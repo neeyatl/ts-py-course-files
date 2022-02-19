609256be69861efeca66d29e166cfb0138d0d334
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-02-19 17:38:39 +0530

    Slicing challenge

---
+++ 01 Intro/05-back.py
@@ -0,0 +1,8 @@
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
+print(alphabets[::-1])  #python idiom of reversing
+
+
+# Challenge
+print(alphabets[-10:-13:-1])
+print(alphabets[4::-1])
+print(alphabets[-1:-9:-1])
