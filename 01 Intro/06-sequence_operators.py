4a0557384d35cfe2d20e65253eaf23854e167cd4
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-02-19 17:49:18 +0530

    Working with Sequence Operators

---
+++ 01 Intro/06-sequence_operators.py
@@ -0,0 +1,10 @@
+peripherals = ["CPU", "Mouse", "Monitor", "Keyboard", "Mouse Pad"]
+print(peripherals)
+
+print("Yo! " * 5)
+
+# in operator, works like substring function in other PLs
+today = "Saturday"
+print('day' in today)
+print('Sat' in today)
+print('sat' in today)
