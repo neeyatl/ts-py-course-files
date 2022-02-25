369a980fe7e8656860ecbdb2e831f42cf8ba68fa
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-02-25 17:05:34 +0530

    Trying shortcut boolean expression with if

---
+++ 02 Flow Control/03-truthy.py
@@ -0,0 +1,12 @@
+natural_num = int(input("Enter a natural number: "))
+if natural_num:
+    print("{} is a natural number.".format(natural_num))
+else:
+    print("Not a natural number")
+
+
+name = input("Enter your name: ")
+if name:
+    print("Hello",name)
+else:
+    print('Do you prefer numbers?')
