d3cc06c48cf4fb89249bbfc230caa3cbe2d922e9
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-03-14 09:38:08 +0530

    Remove values from a list

---
+++ 03 Sequence Types/05-remove_values.py
@@ -0,0 +1,28 @@
+numbers = [
+    30, 38, 102, 109, 119, 123, 176, 189, 183, 198, 199, 201, 262
+]
+
+min_val = 100
+max_val = 200
+
+# trim smaller values
+stop = None
+for i, num in enumerate(numbers):
+    if num >= min_val:
+        stop = i
+        break
+
+if stop is not None:
+    del numbers[:stop]
+print(numbers)
+
+# trim larger values
+start = None
+for i in range(len(numbers) - 1,-1,-1):
+    if numbers[i] <= max_val:
+        start = i + 1   # + 1 because we want to include the max_val or any value less than max_val
+        break
+
+if start is not None:
+    del numbers[start:]
+print(numbers)
